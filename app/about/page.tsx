9ae624bc3b952c7a472bcf18e1323dc33d057d1d
Anuj <anuj89806@gmail.com>
2026-03-30 18:20:23 +0530

    setup prisma

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,20 @@
-const AboutPage = () => {
-  return <div>AboutPage</div>;
+import prisma from "@/utils/db";
+
+const AboutPage = async () => {
+  const posts = await prisma.user.findMany();
+
+  return (
+    <div>
+      {posts.map((post) => {
+        return (
+          <div key={post.id}>
+            <h1 className="text-3xl">{post.name}</h1>
+            <p className="text-lg">{post.email}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default AboutPage;
