25f7e3fc2353580279295a23b2bc33ae32568486
Anuj <anuj89806@gmail.com>
2026-03-30 10:19:03 +0530

    added shadcn theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Container from "@/components/global/Container";
+import Providers from "./providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
   subsets: ["latin"],
 });
 
@@ -36,11 +33,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <Providers>
+          <Navbar />
+          <Container className="py-20">{children}</Container>
+        </Providers>
+      </body>
     </html>
   );
 }
--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,5 +1,15 @@
+import { Input } from "../ui/input";
+
 const NavSearch = () => {
-  return <div>NavSearch</div>;
+  return (
+    <div>
+      <Input
+        type="search"
+        placeholder="search products..."
+        className="max-w-xs dark:bg-muted"
+      />
+    </div>
+  );
 };
 
 export default NavSearch;
