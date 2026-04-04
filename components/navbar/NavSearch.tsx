4b1acbc9d97f6094f2c8d462c670871d6adc9632
Anuj <anuj89806@gmail.com>
2026-04-05 05:28:08 +0530

    added search functionality and fetchSingleProduct function

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,12 +1,43 @@
+"use client";
 import { Input } from "../ui/input";
+import { useSearchParams, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+import { useState, useEffect } from "react";
 
 const NavSearch = () => {
+  const searchParams = useSearchParams();
+  const searchValue = searchParams.get("search")?.toString() || "";
+  const { replace } = useRouter();
+  const [search, setSearch] = useState(searchValue);
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    console.log(params);
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+    replace(`/products?${params.toString()}`);
+  }, 500);
+
+  useEffect(() => {
+    if (!searchParams.get("search")) {
+      setSearch("");
+    }
+  }, [searchParams.get("search")]);
+
   return (
     <div>
       <Input
         type="search"
         placeholder="search products..."
         className="max-w-xs dark:bg-muted"
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+          handleSearch(e.target.value);
+        }}
       />
     </div>
   );
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,4 +1,5 @@
 import db from "@/utils/db";
+import { redirect } from "next/navigation";
 
 export const fetchFeaturedProducts = async () => {
   const products = await db.products.findMany({
@@ -9,10 +10,26 @@ export const fetchFeaturedProducts = async () => {
   return products;
 };
 
-export const fetchAllProducts = () => {
+export const fetchAllProducts = ({ search = "" }: { search: string }) => {
   return db.products.findMany({
+    where: {
+      OR: [
+        { name: { contains: search, mode: "insensitive" } },
+        { company: { contains: search, mode: "insensitive" } },
+      ],
+    },
     orderBy: {
       createdAt: "desc",
     },
   });
+};
+
+export const fetchSingleProduct = async (productId: string) => {
+  const product = await db.products.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+  if (!product) redirect("/products");
+  return product;
 };
