a4d7f1b3018d9b229e6bb0d6d724def950503157
Anuj <anuj89806@gmail.com>
2026-04-05 04:25:11 +0530

    created productContainer Component

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,16 @@
-const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+import ProductsContainer from "@/components/products/ProductsContainer";
+
+const ProductsPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ layout?: string; search?: string }>;
+}) => {
+  const resolvedParams = await searchParams;
+
+  const layout = resolvedParams.layout || "grid";
+  const search = resolvedParams.search || "";
+
+  return <ProductsContainer layout={layout} search={search} />;
 };
 
 export default ProductsPage;
