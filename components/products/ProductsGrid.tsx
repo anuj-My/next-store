90b952dfd795c1832e3d66707c98735e9001651f
Anuj <anuj89806@gmail.com>
2026-04-04 22:30:55 +0530

    created featured products component and fixed db connection issue

--- components/products/ProductsGrid.tsx
+++ components/products/ProductsGrid.tsx
@@ -3,6 +3,7 @@ import { Product } from "@prisma/client";
 import Link from "next/link";
 import { Card, CardContent } from "../ui/card";
 import Image from "next/image";
+import FavoriteToggleButton from "./FavoriteToggleButton";
 
 const ProductsGrid = ({ products }: { products: Product[] }) => {
   return (
@@ -27,9 +28,19 @@ const ProductsGrid = ({ products }: { products: Product[] }) => {
                       className="rounded w-full object-cover transform group-hover:scale-110 transition-transform duration-500"
                     />
                   </div>
+
+                  <div className="mt-4 text-center">
+                    <h2 className="text-xl capitalize">{name}</h2>
+                    <p className="text-muted-foreground mt-2">
+                      {dollarsAmount}
+                    </p>
+                  </div>
                 </CardContent>
               </Card>
             </Link>
+            <div className="absolute top-7 right-7 z-5">
+              <FavoriteToggleButton productId={productId} />
+            </div>
           </article>
         );
       })}
