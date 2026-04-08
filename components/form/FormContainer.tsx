8a0d273d5cd9136082a19223252fa0cc4c11be1d
Anuj <anuj89806@gmail.com>
2026-04-08 09:56:14 +0530

    created createProduct page and implemented zod validation

---
+++ components/form/FormContainer.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useFormState } from "react-dom";
+import { useActionState, useEffect } from "react";
+import { toast } from "sonner";
+import { actionFunction } from "@/utils/types";
+
+const initialState = {
+  message: "",
+};
+
+const FormContainer = ({
+  action,
+  children,
+}: {
+  action: actionFunction;
+  children: React.ReactNode;
+}) => {
+  const [state, formAction] = useActionState(action, initialState);
+
+  useEffect(() => {
+    if (state.message) {
+      toast(state.message);
+    }
+  }, [state.message]);
+
+  return <form action={formAction}>{children}</form>;
+};
+
+export default FormContainer;
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,21 @@
+"use server";
+
 import db from "@/utils/db";
+import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { productSchema, validateWithSchema } from "./schemas";
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) redirect("/");
+  return user;
+};
+
+const renderError = (error: unknown): { message: string } => {
+  return {
+    message: error instanceof Error ? error.message : "something went wrong",
+  };
+};
 
 export const fetchFeaturedProducts = async () => {
   const products = await db.products.findMany({
@@ -10,8 +26,8 @@ export const fetchFeaturedProducts = async () => {
   return products;
 };
 
-export const fetchAllProducts = ({ search = "" }: { search: string }) => {
-  return db.products.findMany({
+export const fetchAllProducts = async ({ search = "" }: { search: string }) => {
+  return await db.products.findMany({
     where: {
       OR: [
         { name: { contains: search, mode: "insensitive" } },
@@ -32,4 +48,26 @@ export const fetchSingleProduct = async (productId: string) => {
   });
   if (!product) redirect("/products");
   return product;
+};
+
+export const createProductAction = async (
+  prevState: any,
+  formData: FormData,
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+  try {
+    const rawData = Object.fromEntries(formData.entries());
+    const validatedFields = validateWithSchema(productSchema, rawData);
+
+    await db.products.create({
+      data: {
+        ...validatedFields,
+        image: "/images/product-3.jpg",
+        clerkId: user.id,
+      },
+    });
+    return { message: "product created" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
